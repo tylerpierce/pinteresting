798d9bcfd58d2f21e65c5300296d318304f1fab2
Ethan Jones <ethangj@gmail.com>
2016-08-25 17:08:41 -0400

    fixing things

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,6 @@
-require 'test_helper'
+require 'tsldifjsldkfjlsdfkljest_helper'
 
-class UserTest < ActiveSupport::TestCase
+class UserTeslksdlkfslkjdfslkjfdlsjdflksst < AcdnfsjdkfnjskdfknjsdtiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
